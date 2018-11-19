5bbe4b1b87907b263eed7d121abf9be0e43c49d1
borjacamserepos <borjacamse@gmail.com>
2018-11-19 16:33:14 +0100

    more

--- testeos/src/test/java/example/IETest.java
+++ testeos/src/test/java/example/IETest.java
@@ -2,19 +2,17 @@ package example;
 
 import java.io.File;
 import java.util.Date;
-import java.util.List;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.FileAppender;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.log4j.SimpleLayout;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class IETest {
@@ -41,7 +39,8 @@ public class IETest {
 		System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
 		WebDriver driver = new InternetExplorerDriver();
 		driver.get(google);
-		driver.navigate().to("http://www.google.com");
+		driver.navigate().to(google);
+		System.out.println("Navigated to " + google);
 		// Selenium actions
 		/*
 		driver.navigate().to(google);
@@ -55,6 +54,34 @@ public class IETest {
 			OurLogger.info(date + " Registered: " + i + ". Found:" + results.get(i).getText());
 		}*/
 
+	}
+	
+		public WebDriver driver;
+		
+		@BeforeClass
+		public void setUp() {
+			System.out.println("*******************");
+			System.out.println("launching IE browser");
+			System.setProperty("webdriver.ie.driver", "C:\\Driver\\iedriver.exe");
+			driver = new InternetExplorerDriver();
+			driver.manage().window().maximize();
+		}
+		
+		@Test
+		public void testGooglePageTitleInIEBrowser() {
+			driver.navigate().to("http://www.google.com");
+			String strPageTitle = driver.getTitle();
+			System.out.println("Page title: - "+strPageTitle);
+			Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google"), "Page title doesn't match");
+		}
+
+		@AfterClass
+		public void tearDown() {
+			if(driver!=null) {
+				System.out.println("Closing IE browser");
+				driver.quit();
+			}
+		
 	}
 
 }
