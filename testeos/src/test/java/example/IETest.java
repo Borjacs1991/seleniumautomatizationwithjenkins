6a98f99920097bfef1756d9a28a9a28ebf4912d2
borjacamserepos <borjacamse@gmail.com>
2018-11-19 14:18:23 +0100

    more more

--- testeos/src/test/java/example/IETest.java
+++ testeos/src/test/java/example/IETest.java
@@ -1,5 +1,6 @@
 package example;
 
+import java.io.File;
 import java.util.Date;
 import java.util.List;
 
@@ -16,41 +17,29 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class IETest {
-	
+
 	private final String google = "https://www.google.es/";
-	
-	  @Test
-	  public void sendTestIE() {
-		  // Logger
-			Date date = new Date();
-			BasicConfigurator.configure();
-			Logger OurLogger = LogManager.getLogger("OurLogger");
-			
-		    FileAppender fileAppender = new FileAppender();
-		    fileAppender.setFile("IELogfile.txt");
-		    fileAppender.setLayout(new SimpleLayout());
-		 
-		    OurLogger.addAppender(fileAppender);
-		    fileAppender.activateOptions();
-		    
-		    // Selenium
-			System.setProperty("webdriver.ie.driver","C:\\Driver\\IEDriverServer.exe");
-			WebDriver driver = new InternetExplorerDriver();
-			
-			// Selenium actions
-			driver.navigate().to(google);
-			driver.findElement(By.name("q")).sendKeys("Spring");
-			driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-			
-			List<WebElement> results = driver.findElements(By.tagName("h3"));
-			
-			for(int i = 0; i<results.size(); i++) {
-				System.out.println("Found: " + results.get(i).getText());
-				OurLogger.info(date + " Registered: " + i + ". Found:" + results.get(i).getText());
-			}
-			
-			driver.close();
-			driver.quit();
-	  }
+
+	@Test
+	public void sendTestIE() {
+
+		// Selenium
+		File file = new File("C:\\\\Driver\\\\IEDriverServer.exe");
+		System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
+		WebDriver driver = new InternetExplorerDriver();
+
+		// Selenium actions
+		driver.navigate().to(google);
+		driver.findElement(By.name("q")).sendKeys("Spring");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+		List<WebElement> results = driver.findElements(By.tagName("h3"));
+
+		for (int i = 0; i < results.size(); i++) {
+			System.out.println("Found: " + results.get(i).getText());
+		}
+
+		driver.close();
+	}
 
 }
